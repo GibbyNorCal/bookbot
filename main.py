0bf8250020114837ac0fe68d8b25f14c64e0d633
GibbyNorCal <kgibby77@gmail.com>
2024-08-22 13:07:09 -0700

    counting adding

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def main():
-    with open(books/frankenstein.txt) as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    text = book_contents(book_path)
+    num_words = book_length(text)
+    print(f"This book has", num_words, "words")
+
+def book_length(file_contents):
+    words = file_contents.split()
+    return len(words)
+
+def book_contents(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+
+
+
+
 
 main()
